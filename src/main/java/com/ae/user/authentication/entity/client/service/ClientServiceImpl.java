15b39071cebcbc5b9cb18015922a5e9ce2fca621
Andres <andres.rivas@mercadolibre.com>
2020-07-03 19:07:05 -0300

    Implement client service

--- src/main/java/com/ae/user/authentication/entity/client/service/ClientServiceImpl.java
+++ src/main/java/com/ae/user/authentication/entity/client/service/ClientServiceImpl.java
@@ -1,13 +1,17 @@
 package com.ae.user.authentication.entity.client.service;
 
 import com.ae.user.authentication.entity.client.repository.ClientRepository;
+import com.ae.user.authentication.model.ClientModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
-public class ClientServiceImpl {
+public class ClientServiceImpl implements ClientService {
 
-    private ClientRepository repository;
+    private final ClientRepository repository;
 
     @Autowired
     public ClientServiceImpl(final ClientRepository repository) {
@@ -15,5 +19,46 @@ public class ClientServiceImpl {
     }
 
 
+    @Transactional
+    @Override
+    public String createClient(final ClientModel client) {
+        repository.save(client);
+        return "done";
+    }
 
+    @Transactional(readOnly = true)
+    @Override
+    public ClientModel getClient(final ClientModel client) {
+        return repository.findById(client.getId()).get();
+    }
+
+    @Transactional(readOnly = true)
+    @Override
+    public List<ClientModel> getClients() {
+        return (List<ClientModel>) repository.findAll();
+    }
+
+    @Transactional()
+    @Override
+    public ClientModel updateClient(final ClientModel client) {
+        if (validateClient(client)) {
+            repository.save(client);
+            return getClient(client);
+        }
+        return null;
+    }
+
+    @Transactional()
+    @Override
+    public String deleteClient(final ClientModel client) {
+        if (validateClient(client)) {
+            repository.delete(client);
+            return "done";
+        }
+        return "not done";
+    }
+
+    private Boolean validateClient(final ClientModel client) {
+        return getClient(client) != null;
+    }
 }
