19753a6ad55e89bf1364569da5332e0bbacb9a96
Andres <andres.rivas@mercadolibre.com>
2020-07-03 18:56:10 -0300

    Added methods to client service

--- src/main/java/com/ae/user/authentication/entity/client/service/ClientService.java
+++ src/main/java/com/ae/user/authentication/entity/client/service/ClientService.java
@@ -1,6 +1,41 @@
 package com.ae.user.authentication.entity.client.service;
 
+import com.ae.user.authentication.model.ClientModel;
+
+import java.util.List;
+
+/**
+ * Interface to make interaction with Repository, CRUD operation
+ */
 public interface ClientService {
 
+    /**
+     * @param client to save in data base
+     * @return Message about transaction
+     */
+    String createClient(final ClientModel client);
+
+    /**
+     * @param client to find in data base
+     * @return client found
+     */
+    ClientModel getClient(final ClientModel client);
+
+    /**
+     * @return List of clients registered
+     */
+    List<ClientModel> getClients();
+
+    /**
+     * @param client to uodate
+     * @return updated client
+     */
+    ClientModel updateClient(final ClientModel client);
+
+    /**
+     * @param client to delete
+     * @return deleted client
+     */
+    String deleteClient(final ClientModel client);
 
 }
